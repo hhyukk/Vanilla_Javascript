e8d6832ce429f86aa6a07122d0a67cbf1bb2cbc4
hhyukk <hhyukk1273@gmail.com>
2023-07-02 14:17:45 +0900

    Events
    form을 submit하면 브라우저는 기본적으로 페이지를 새로고침 하도록 되어있다.
    preventDefault() 함수를 추가함으로써 브라우저의 기본 동작을 막을 수 있다
    
    preventDefault 함수는 EventListener 함수의 '첫 번째 argument' 안에 있는 함수이다. 첫 arument는 지금 막 벌어진 event들에 대한 정보를 갖고 있다.
    JS는(기본적으로)argument를 담아서 함수를 호출하는데, 이 argument가 기본 정보들을 제공하고 있다. ex) 누가 submit주체인지, 몇 시에 submit을 했는지 등등 콘솔에 출력해보면 알 수 있음

--- Login/app.js
+++ Login/app.js
@@ -1,8 +1,11 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
+function onLoginSubmit(event) {
+  event.preventDefault(); // preventDefault는 어떤 event의 기본 행동이 발생되지 않도록 막는 것
+  console.log(loginInput.value);
+  // const username = loginInput.value;
+  // console.log(username);
 }
-
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+// JS는 onLoginSubmit함수 호출시 인자를 담아서 호출함. 해당 인자는 event object를 담은 정보들
