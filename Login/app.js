f66759c22944b23d9566aaccfbe2769dee39b0c9
hhyukk <hhyukk1273@gmail.com>
2023-07-02 15:56:45 +0900

    Events part Two
    addEventListener 안에 있는 함수는 직접 실행하지 않는다
    브라우저가 실행시켜주고
    브라우저에서 해당 이벤트에 대한 정보 즉, object를 가지게 된다.
    addEventListener의 함수에서 object에 대한 자리만 할당해주면
    해당 이벤트가 발생시킨 정보들에 대한 object들을 볼 수 있다!
    이때 해당 이벤트가 가진 기본 Default값을 발생시키지 않기 하게 위해선 preventDefault를 이용하여 막을 수 있다!

--- Login/app.js
+++ Login/app.js
@@ -1,11 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault(); // preventDefault는 어떤 event의 기본 행동이 발생되지 않도록 막는 것
   console.log(loginInput.value);
   // const username = loginInput.value;
   // console.log(username);
 }
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
 loginForm.addEventListener("submit", onLoginSubmit);
 // JS는 onLoginSubmit함수 호출시 인자를 담아서 호출함. 해당 인자는 event object를 담은 정보들
+link.addEventListener("click", handleLinkClick);
