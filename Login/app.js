a14b95ddde866634ad3b2a8a1f69487a5cd7b7bb
hhyukk <hhyukk1273@gmail.com>
2023-07-02 13:48:09 +0900

    Form Submission
    JavaScript에서 구현하는 대신 할 수 있다면 HTML의 기본 속성을 최대한 이용하는게 좋다.

--- Login/app.js
+++ Login/app.js
@@ -2,13 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  const username = loginInput.ariaValueMax;
-  if (username === "") {
-    alert("please write your name");
-  } else if (username.length > 15) {
-    //username이 15자 이상이면 경고창을 보여줌
-    alert("Your name is too long.");
-  }
+  const username = loginInput.value;
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
