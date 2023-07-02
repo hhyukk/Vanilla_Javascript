e16159bb8b91244cbe58384b918bd6d2fbc21aba
hhyukk <hhyukk1273@gmail.com>
2023-07-02 16:31:45 +0900

    Getting Username
    const HIDDEN_CLASSNAME = "hidden";
    // 일반적으로 string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용한다.
    
    greeting.innerText = `Hello ${username}`;
      // "Hello " + username과 같다.

--- Login/app.js
+++ Login/app.js
@@ -1,18 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
-  event.preventDefault(); // preventDefault는 어떤 event의 기본 행동이 발생되지 않도록 막는 것
-  console.log(loginInput.value);
-  // const username = loginInput.value;
-  // console.log(username);
-}
-function handleLinkClick(event) {
   event.preventDefault();
-  console.dir(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  console.log(username);
+
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 loginForm.addEventListener("submit", onLoginSubmit);
-// JS는 onLoginSubmit함수 호출시 인자를 담아서 호출함. 해당 인자는 event object를 담은 정보들
-link.addEventListener("click", handleLinkClick);
